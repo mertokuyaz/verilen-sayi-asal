1f1b090e51585139d33b4341354c02e18cbfcf38
okuyaz <22006512+okuyaz@users.noreply.github.com>
2022-05-13 19:32:30 +0300

    Update asalsayibulucu.py

--- asalsayibulucu.py
+++ asalsayibulucu.py
@@ -1,19 +1,20 @@
-sayı = int(input("Lütfen bir sayı giriniz: "))
+#1 asal sayı değildir!
+sayı = int(input("Lütfen bir pozitif sayı giriniz: "))
 
 def ran(start, end):
     return range(start, end+1)
 
     #alternatif olarak for loop için de yazılabilir
     #o sayiya kadar olan bütün sayıları o sayının kendisiyle böl gibi.
-    #eğer bölünebilen sayı varsa print(num, "asal sayi degil") döngüsünü başlat.
+    #eğer bölünebilen sayı varsa print(num, "bir asal sayi degil") döngüsünü başlat.
 
 if sayı > 1:  
    for i in ran(2, sayı//2): 
        if (sayı % i) == 0: 
-           print(sayı, "asal sayı değil") 
+           print(sayı, "bir asal sayı değil") 
            break
    else: 
-       print(sayı, "asal sayı") 
+       print(sayı, "bir asal sayı") 
   
 else: 
-   print(sayı, "asal sayı") 
+   print(sayı, "bir asal sayı") 
